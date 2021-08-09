0a6946dd9f2c5376b1839c3f804ac8f42306622c
Roberto Guerra <uris77@gmail.com>
2021-08-09 16:59:36 -0600

    labels months instead of weeks

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,45 @@ function App() {
               />
               <VictoryAxis
                 style={{ tickLabels: { angle: -60 } }}
-                tickFormat={(x) => x.split('-')[1]}
+                tickFormat={(x) => {
+                  // divide by 4 to estimate months, and return
+                  // the name of the month for the first value.
+                  const [year, week] = x.split('-');
+                  console.log(year);
+                  let month = 'Jan';
+                  switch (week) {
+                    case '1':
+                      month = 'Jan';
+                      break;
+                    case '5':
+                      month = 'Feb';
+                      break;
+                    case '9':
+                      month = 'Mar';
+                      break;
+                    case '13':
+                      month = 'Apr';
+                      break;
+                    case '17':
+                      month = 'May';
+                      break;
+                    case '22':
+                      month = 'Jun';
+                      break;
+                    case '26':
+                      month = 'Jul';
+                      break;
+                    case '31':
+                      month = 'Aug';
+                      break;
+                    case '35':
+                      month = 'Sep';
+                      break;
+                    default:
+                      month = '';
+                  }
+                  return month;
+                }}
               />
               <VictoryAxis dependentAxis />
               <VictoryArea
